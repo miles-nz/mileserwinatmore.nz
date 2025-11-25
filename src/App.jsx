2c6765987c0d7a1055e401d443fb0e6b6798ce24
Miles Erwin-Atmore <merw957@aucklanduni.ac.nz>
2025-11-25 16:10:24 +1300

    Update animation, add css breakpoints for font size, change page title

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react";
+import { MdRotateRight } from "react-icons/md";
+import { ImSpinner11 } from "react-icons/im";
 import "./App.css";
 
 function Header({ title }) {
@@ -11,7 +13,14 @@ function Button({ onClick, classes }) {
     return (
         <div className={classes}>
             <button onClick={onClick} className="button">
-                spin!
+                <motion.div
+                    animate={{
+                        rotate: 360,
+                        transition: { repeat: Infinity, duration: 5 },
+                    }}
+                >
+                    <ImSpinner11 />
+                </motion.div>
             </button>
         </div>
     );
@@ -24,11 +33,37 @@ function App() {
     }
     return (
         <div className="centered-container">
-            <motion.div key={count} animate={{ rotate: 360 }}>
-                <div className="centered-group">
-                    <Header title="Miles Erwin-Atmore" />
-                    <Button onClick={handleClick} />
-                </div>
+            <motion.div
+                initial={{ x: -360, y: -360, scale: 0.3 }}
+                animate={{
+                    x: 0,
+                    y: 0,
+                    scale: 1,
+                    transition: {
+                        type: "spring",
+                        mass: 0.5,
+                        damping: 2,
+                        stiffness: 15,
+                    },
+                }}
+            >
+                <motion.div
+                    key={count}
+                    animate={{
+                        rotate: 360,
+                        transition: {
+                            type: "spring",
+                            mass: 0.5,
+                            damping: 2,
+                            stiffness: 15,
+                        },
+                    }}
+                >
+                    <div className="centered-group">
+                        <Header title="Miles Erwin-Atmore" />
+                        <Button onClick={handleClick} />
+                    </div>
+                </motion.div>
             </motion.div>
         </div>
     );
